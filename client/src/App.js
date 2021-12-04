f6adb444f64db0972f0dbd04472baa362022482b
nicodeheza <nicolasdeheza@hotmail.com>
2021-12-04 17:22:26 -0300

    url prod and dev

--- client/src/App.js
+++ client/src/App.js
@@ -1,44 +1,33 @@
-import React, { useEffect } from 'react';
-import Home from './components/home/Home';
-import Loading from './components/loading/Loading';
-import Principal from './components/principal/Principal';
-import {Provider, useSelector, useDispatch} from 'react-redux';
-import generateStore from './redux/store';
-import { getAuth } from './redux/actions/authActions';
+import React, {useEffect} from "react";
+import Home from "./components/home/Home";
+import Loading from "./components/loading/Loading";
+import Principal from "./components/principal/Principal";
+import {Provider, useSelector, useDispatch} from "react-redux";
+import generateStore from "./redux/store";
+import {getAuth} from "./redux/actions/authActions";
 
 function Main() {
-  const auth= useSelector(store=> store.auth.authData);
+	const auth = useSelector((store) => store.auth.authData);
 
-  const dispatch= useDispatch();
+	const dispatch = useDispatch();
 
-  //check auth in the server
-  useEffect(()=>{
-    if(auth === undefined){
-      dispatch(getAuth());
-    }
-  });
+	//check auth in the server
+	useEffect(() => {
+		if (auth === undefined) {
+			dispatch(getAuth());
+		}
+	});
 
-  return (
-    <>
-    {auth === undefined ?
-    (<Loading/>) :
-    auth ? 
-    (<Principal/>)
-    :
-    (<Home/>)
-    }  
-    </>
-  );
+	return <>{auth === undefined ? <Loading /> : auth ? <Principal /> : <Home />}</>;
 }
 
 function App() {
-  const store= generateStore();
-  return (
-    <Provider store={store}>
-      <Main /> 
-    </Provider>
-  );
+	const store = generateStore();
+	return (
+		<Provider store={store}>
+			<Main />
+		</Provider>
+	);
 }
 
 export default App;
-
--- client/src/serverUrl.js
+++ client/src/serverUrl.js
@@ -1,3 +1,6 @@
 //const SERVER_URL = "http://localhost:4000/api";
-const SERVER_URL = "http://localhost:3000/api";
+const SERVER_URL =
+	process.env.NODE_ENV === "production"
+		? "http://localhost:3000/api"
+		: "http://localhost:4000/api";
 export default SERVER_URL;
