3395a8c31ccf747683612c93ad95d2eb9937c294
nicodeheza <nicolasdeheza@hotmail.com>
2021-12-21 12:12:43 -0300

    client fetch url update

--- client/src/serverUrl.js
+++ client/src/serverUrl.js
@@ -1,6 +1,5 @@
-//const SERVER_URL = "http://localhost:4000/api";
 const SERVER_URL =
 	process.env.NODE_ENV === "production"
-		? "http://localhost:3000/api"
+		? `${window.location.protocol}//${window.location.host}/api`
 		: "http://localhost:4000/api";
 export default SERVER_URL;
