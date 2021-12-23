b5385181a62837001d1dce10ace7fa1daff60406
nicodeheza <nicolasdeheza@hotmail.com>
2021-12-23 15:37:44 -0300

    ssl_renew update

--- ssl_renew.sh
+++ ssl_renew.sh
@@ -4,5 +4,5 @@ COMPOSE="/usr/local/bin/docker-compose --no-ansi"
 DOCKER="/usr/bin/docker"
 
 cd /home/ubuntu/happy-day/
-$COMPOSE run certbot renew --dry-run && $COMPOSE kill -s SIGHUP webserver
+$COMPOSE run certbot renew --dry-run && $COMPOSE kill -s SIGHUP nginx
 $DOCKER system prune -af
