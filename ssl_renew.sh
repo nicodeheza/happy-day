5fb1d30707122c61c0637a47416d9dd2e54ba830
nicodeheza <nicolasdeheza@hotmail.com>
2021-12-23 15:13:17 -0300

    ssl_renew correccion

--- ssl_renew.sh
+++ ssl_renew.sh
@@ -4,5 +4,5 @@ DOCKER="/usr/bin/docker"
 
 cd /home/ubuntu/happy-day/
 
-$DOCKER sudo docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v "$PWD:$PWD" --env MONGO_INITDB_ROOT_USERNAME --env MONGO_INITDB_ROOT_PASSWORD --env SECRET --env SENDGRID_API_KEY --env PUBLIC_VAPID_KEY --env PRIVATE_VAPID_KEY -w="$PWD" linuxserver/docker-compose run certbot renew --dry-run && $DOCKER sudo docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v "$PWD:$PWD" --env MONGO_INITDB_ROOT_USERNAME --env MONGO_INITDB_ROOT_PASSWORD --env SECRET --env SENDGRID_API_KEY --env PUBLIC_VAPID_KEY --env PRIVATE_VAPID_KEY -w="$PWD" linuxserver/docker-compose kill -s SIGHUP nginx
+$DOCKER run --rm -v /var/run/docker.sock:/var/run/docker.sock -v "$PWD:$PWD" --env MONGO_INITDB_ROOT_USERNAME --env MONGO_INITDB_ROOT_PASSWORD --env SECRET --env SENDGRID_API_KEY --env PUBLIC_VAPID_KEY --env PRIVATE_VAPID_KEY -w="$PWD" linuxserver/docker-compose run certbot renew --dry-run && $DOCKER run --rm -v /var/run/docker.sock:/var/run/docker.sock -v "$PWD:$PWD" --env MONGO_INITDB_ROOT_USERNAME --env MONGO_INITDB_ROOT_PASSWORD --env SECRET --env SENDGRID_API_KEY --env PUBLIC_VAPID_KEY --env PRIVATE_VAPID_KEY -w="$PWD" linuxserver/docker-compose kill -s SIGHUP nginx
 $DOCKER system prune -af
